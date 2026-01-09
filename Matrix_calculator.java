8af9cb97195eb37a5a4bfbcd5fb763e2bc67a12d
Ahmed Turaysh <AhmedTresh38@gmail.com>
2026-01-09 23:30:03 +0300

    fixed structure

--- Matrix_calculator.java
+++ Matrix_calculator.java
@@ -1,9 +1,26 @@
-import java.util.Scanner;
+
 public class Matrix_calculator {
-    public static void main(String[]agrs){
+    private int rows;
+    private int cols;
 
-
-        
+private Matrix_calculator(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
     }
-    
+private int[][] multiply(int[][] a, int[][] b) {
+        int[][] result = new int[rows][cols];
+
+        return result;
+    }
+    private int [][]add(int [][]a, int[][]b){
+
+
+        int [][]result = new int [rows][cols];
+        return result;
+    }
+    private int [][]subtract(int [][]a, int[][]b){
+
+        int [][]result = new int [rows][cols];
+        return result;
+    }
 }
